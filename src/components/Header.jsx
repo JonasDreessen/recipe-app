ab37ae367c71dae582aa09178098ee875c0d76d5
JonasDreessen <dreessenjonas95@gmail.com>
2019-09-20 16:54:31 +0200

    added navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+function Header () {
+    return(
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>How it works</li>
+                <li>Recipes</li>
+                <li>need to improvise</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
